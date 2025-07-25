ad48bfb0790ed54fab4482ea09a2890191cbf292
ANNU3216 <ansar825550@gmail.com>
2025-07-25 21:05:13 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,1 @@
-// supabase.js for plain HTML/JS with CDN
-const SUPABASE_URL = 'https://bzrcawqsbahxjliqlndb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6cmNhd3FzYmFoeGpsaXFsbmRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMjI4OTgsImV4cCI6MjA2NTg5ODg5OH0._7zsMUy2-MsD6d2UUn31LguYCx6FZaB1ixGsiEyiYwU';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
